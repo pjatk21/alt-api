ce4457f8b38ad60a2d07ff6e5bfd23874035c7f0
Krystian Postek <krystian@postek.eu>
2022-02-15 18:53:16 +0100

    Switch to fastify, add OpenAPI explorer, disable semi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
+
+  // Swagger init
+  SwaggerModule.setup(
+    'docs',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Alternative PJATK API')
+        .setDescription(
+          'Scrapper based alternative for blocked and outdated APIs.',
+        )
+        .build(),
+    ),
+  )
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
