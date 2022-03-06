f45d58100d1bda1d1b045937782e723c3e8d66a7
Krystian Postek <krystian@postek.eu>
2022-03-06 13:26:11 +0100

    Force luxon to use browser's locale

--- alt-api-ui/src/main.tsx
+++ alt-api-ui/src/main.tsx
@@ -1,9 +1,13 @@
+import { Settings } from 'luxon'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
 // wtf, is this a valid solution??
 window.React = React
+
+// set locale
+Settings.defaultLocale = navigator.language
 
 ReactDOM.render(
   <React.StrictMode>
