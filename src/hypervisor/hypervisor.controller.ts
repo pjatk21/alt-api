b80ff1dd87093d60e600b3464300f85a04e4649c
Krystian Postek <krystian@postek.eu>
2022-03-07 01:41:19 +0100

    Minor improvments

--- src/hypervisor/hypervisor.controller.ts
+++ src/hypervisor/hypervisor.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, ParseEnumPipe, Post } from '@nestjs/common'
 import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { ConnectedSocket, WebSocketServer } from '@nestjs/websockets'
 import EventEmitter from 'events'
 import { Server } from 'socket.io'
 import { Socket } from 'socket.io-client'
-import { HypervisorEvents } from './hypervisor.enum'
+import { HypervisorEvents, HypervisorScrapperCommands } from './hypervisor.enum'
 import { HypervisorService } from './hypervisor.service'
+import { HypervisorCommandExec } from './hypervisor.types'
 
 @Controller('hypervisor')
 @ApiTags('Hypervisor')
 export class HypervisorController {
   constructor(private hypervisor: HypervisorService) {}
+
+  @Get('scrappers')
+  async scrappers() {
+    return []
+  }
 
   @Post('visa/accept')
   @ApiOperation({
@@ -28,8 +34,14 @@ export class HypervisorController {
   @ApiOperation({
     summary: 'Manage with working scrappers',
   })
-  async manage(@Body('socketId') socketId: string) {
-    this.hypervisor.socket.to(socketId).emit(HypervisorEvents.COMMAND, 'disconnect')
+  async manage(
+    @Body('socketId') socketId: string,
+    @Body('command', new ParseEnumPipe(HypervisorScrapperCommands))
+    command: HypervisorScrapperCommands,
+  ) {
+    this.hypervisor.socket.to(socketId).emit(HypervisorEvents.COMMAND, {
+      command,
+    } as HypervisorCommandExec)
     return { staus: 'done!' }
   }
 }
