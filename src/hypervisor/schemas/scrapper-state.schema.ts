d92efc3c515a07d6def6c116ead6931fffe3a672
Krystian Postek <krystian@postek.eu>
2022-03-12 21:12:47 +0100

    Ignore version key

--- src/hypervisor/schemas/scrapper-state.schema.ts
+++ src/hypervisor/schemas/scrapper-state.schema.ts
@@ -1,12 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { ApiProperty } from '@nestjs/swagger'
 import { Document, Types } from 'mongoose'
 import { HypervisorScrapperState } from '../hypervisor.enum'
 import { ScrapperVisa } from './scrapper-visa.schema'
 
 export type ScrapperStateDocument = ScrapperState & Document
 
-@Schema({ timestamps: { createdAt: true, updatedAt: false } })
+@Schema({ timestamps: { createdAt: true, updatedAt: false }, versionKey: false })
 export class ScrapperState {
   @Prop({ required: true, type: Types.ObjectId, ref: 'ScrapperVisa' })
   visa: ScrapperVisa
