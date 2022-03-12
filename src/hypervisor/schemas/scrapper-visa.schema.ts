e0921e1b35d0a90cdef3632521d5eb707d7e3af2
Krystian Postek <krystian@postek.eu>
2022-03-12 20:39:57 +0100

    Remove deprecated props

--- src/hypervisor/schemas/scrapper-visa.schema.ts
+++ src/hypervisor/schemas/scrapper-visa.schema.ts
@@ -4,16 +4,13 @@ import { ScrapperPassportDto } from '../dto/passport.dto'
 
 export type ScrapperVisaDocument = ScrapperVisa & Document
 
-@Schema({ timestamps: true })
+@Schema({ versionKey: false })
 export class ScrapperVisa {
   @Prop({ required: true, type: ScrapperPassportDto })
   passport: ScrapperPassportDto
 
   @Prop({ required: true, default: false })
   active: boolean
-
-  @Prop({ required: true })
-  socketId: string
 }
 
 export const ScrapperVisaSchema = SchemaFactory.createForClass(ScrapperVisa)
