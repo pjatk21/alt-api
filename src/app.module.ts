886ff5c46dccec51603a4e78d8afe2bccfdf029e
Krystian Postek <krystian@postek.eu>
2022-03-12 19:44:47 +0100

    Move throttler to public timetable module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { PublicTimetableModule } from './public-timetable/public-timetable.module'
-import { APP_GUARD } from '@nestjs/core'
 import { PublicTimetableService } from './public-timetable/public-timetable.service'
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
 import { HypervisorModule } from './hypervisor/hypervisor.module'
@@ -14,18 +12,11 @@ import { MongooseModule } from '@nestjs/mongoose'
     MongooseModule.forRoot(process.env.MONGO_URL ?? 'mongodb://localhost/altapi'),
     PublicTimetableModule,
     HypervisorModule,
-    ThrottlerModule.forRoot({ ttl: 600, limit: 3000 }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'alt-api-ui', 'dist'),
     }),
   ],
   controllers: [AppController],
-  providers: [
-    PublicTimetableService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+  providers: [PublicTimetableService],
 })
 export class AppModule {}
--- src/public-timetable/public-timetable.module.ts
+++ src/public-timetable/public-timetable.module.ts
@@ -3,13 +3,22 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { Timetable, TimetableSchema } from './schemas/timetable.schema'
 import { PublicTimetableService } from './public-timetable.service'
 import { PublicTimetableController } from './public-timetable.controller'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Timetable.name, schema: TimetableSchema }]),
     CacheModule.register({ ttl: 60 * 5 }), // 5 minutes
+    ThrottlerModule.forRoot({ ttl: 600, limit: 1200 }), // 2 requests per second
   ],
-  providers: [PublicTimetableService],
+  providers: [
+    PublicTimetableService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   controllers: [PublicTimetableController],
   exports: [
     MongooseModule.forFeature([{ name: Timetable.name, schema: TimetableSchema }]),
