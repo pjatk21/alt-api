8724fc1cf8411e123f0e8cd23e244f2ff544a6bc
Krystian Postek <krystian@postek.eu>
2022-03-19 23:24:55 +0100

    Minor improvements to the scrappers component

--- alt-api-ui/src/scrappers/Scrappers.tsx
+++ alt-api-ui/src/scrappers/Scrappers.tsx
@@ -6,30 +6,35 @@ const baseUrl = import.meta.env.DEV
   ? 'http://krystians-mac-pro.local:4000/'
   : window.location.href
 
-const queryClient = new QueryClient()
-const getScrappers = () => fetch(`${baseUrl}hypervisor/scrappers`).then((r) => r.json())
+type ScrapperLike = {
+  uuid: string
+  name: string
+  lastState: string
+  lastStatus: string
+}
+
+const getScrappers: () => Promise<ScrapperLike[]> = () =>
+  fetch(`${baseUrl}hypervisor/scrappers`).then((r) => r.json())
 
 export function Scrappers() {
-  const queryClient = useQueryClient()
-
   const { data, error, isLoading, isError } = useQuery('scrappers', getScrappers)
 
   if (isLoading) return <Loading />
-  if (isError) return <p>Error</p>
+  if (isError) return <pre>{JSON.stringify(error)}</pre>
 
-  return (
-    <>
-      {data.map((scrapper) => (
-        <p key={scrapper.uuid}>{JSON.stringify(scrapper)}</p>
-      ))}
-    </>
-  )
+  if (data) {
+    return (
+      <>
+        {data.map((scrapper) => (
+          <p key={scrapper.uuid}>{JSON.stringify(scrapper)}</p>
+        ))}
+      </>
+    )
+  } else {
+    return <p>Ayo?</p>
+  }
 }
 
 export function ScrappersWrapper() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Scrappers />
-    </QueryClientProvider>
-  )
+  return <Scrappers />
 }
