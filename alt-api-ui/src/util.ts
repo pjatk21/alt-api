2641a89e615f28e7399a237f0e35b720db29dd54
Krystian Postek <krystian@postek.eu>
2022-03-24 20:43:58 +0100

    Hotfix

--- alt-api-ui/src/util.ts
+++ alt-api-ui/src/util.ts
@@ -1,3 +1,7 @@
 export const baseUrl = import.meta.env.DEV
   ? 'http://krystians-mac-pro.local:4000/'
-  : window.location.href
+  : (() => {
+      const x = new URL(window.location.href)
+      x.pathname = '/'
+      return x
+    })()
