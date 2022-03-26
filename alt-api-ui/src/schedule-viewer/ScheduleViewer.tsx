4d42dae6427670f7aad92410b36c0da44acf1612
Krystian Postek <krystian@postek.eu>
2022-03-26 12:58:40 +0100

    Refactor to increase maintainability

--- alt-api-ui/src/schedule-viewer/ScheduleViewer.tsx
+++ alt-api-ui/src/schedule-viewer/ScheduleViewer.tsx
@@ -2,16 +2,59 @@ import React, { useState } from 'react'
 import { Button, Container, Grid, Input, Spacer, Text } from '@nextui-org/react'
 import { ScheduleTimeline } from './ScheduleTimeline'
 import { DateTime } from 'luxon'
-import { useQueryClient } from 'react-query'
 import { useLocalStorage } from 'usehooks-ts'
 import './ScheduleViewer.sass'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArrowLeft,
+  faArrowRight,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons'
 import { Settings } from './Settings'
 import { GroupPicker } from './GroupPicker'
 
+type DateNaviButtonProps = {
+  icon: IconDefinition
+  onClick: () => void
+}
+
+function DateNaviButton({ icon, onClick }: DateNaviButtonProps) {
+  return <Button bordered auto onClick={onClick} icon={<FontAwesomeIcon icon={icon} />} />
+}
+
+type DateNavigatorProps = {
+  date: DateTime
+  setDate: React.Dispatch<React.SetStateAction<DateTime>>
+}
+
+export function DateNavigator({ date, setDate }: DateNavigatorProps) {
+  return (
+    <Grid.Container gap={1} justify={'center'}>
+      <Grid>
+        <DateNaviButton
+          onClick={() => setDate(date.minus({ day: 1 }))}
+          icon={faArrowLeft}
+        />
+      </Grid>
+      <Grid>
+        <Input
+          bordered
+          type={'date'}
+          onChange={(e) => setDate(DateTime.fromISO(e.target.value))}
+          value={date.toISODate()}
+        />
+      </Grid>
+      <Grid>
+        <DateNaviButton
+          onClick={() => setDate(date.plus({ day: 1 }))}
+          icon={faArrowRight}
+        />
+      </Grid>
+    </Grid.Container>
+  )
+}
+
 export function ScheduleViewer() {
-  const queryClient = useQueryClient()
   const [activeDate, setActiveDate] = useState(DateTime.now())
   const [groups, setGroups] = useLocalStorage<string[]>('groups', [])
   const [groupPickerVisible, setGroupPickerVisible] = useState(groups.length === 0)
@@ -20,35 +63,7 @@ export function ScheduleViewer() {
   return (
     <Container xs>
       <Text h2>Plan zajęć</Text>
-      <Grid.Container gap={1} justify={'center'}>
-        <Grid>
-          <Button
-            bordered
-            auto
-            onClick={() => setActiveDate(activeDate.minus({ day: 1 }))}
-            icon={<FontAwesomeIcon icon={faArrowLeft} />}
-          />
-        </Grid>
-        <Grid>
-          <Input
-            bordered
-            type={'date'}
-            onChange={(e) => {
-              setActiveDate(DateTime.fromISO(e.target.value))
-              queryClient.invalidateQueries({ queryKey: 'schedule' })
-            }}
-            value={activeDate.toISODate()}
-          />
-        </Grid>
-        <Grid>
-          <Button
-            bordered
-            auto
-            onClick={() => setActiveDate(activeDate.plus({ day: 1 }))}
-            icon={<FontAwesomeIcon icon={faArrowRight} />}
-          />
-        </Grid>
-      </Grid.Container>
+      <DateNavigator date={activeDate} setDate={setActiveDate} />
       <ScheduleTimeline date={activeDate} groups={groups} />
       <Spacer />
       <Button auto bordered onClick={() => setGroupPickerVisible(true)}>
