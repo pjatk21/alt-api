1aed72d87f9065862392c9db510483b50de539df
Krystian Postek <krystian@postek.eu>
2022-03-26 15:02:28 +0100

    Add sentry

--- alt-api-ui/src/main.tsx
+++ alt-api-ui/src/main.tsx
@@ -3,6 +3,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { registerSW } from 'virtual:pwa-register'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
+// setup sentry
+if (import.meta.env.PROD)
+  Sentry.init({
+    dsn: 'https://cefc1ce1d2fd4a5ab29407a1392673b5@o1084354.ingest.sentry.io/6290616',
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+    environment: 'PRODUCTION',
+  })
 
 // set locale
 Settings.defaultLocale = navigator.language
