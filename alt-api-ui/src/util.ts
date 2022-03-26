0416c58fc3442b71085978b87ec571468690005a
Krystian Postek <krystian@postek.eu>
2022-03-26 11:11:53 +0100

    Add option to specify URL of API_BASE

--- alt-api-ui/src/util.ts
+++ alt-api-ui/src/util.ts
@@ -1,3 +1,4 @@
 export const baseUrl = import.meta.env.DEV
-  ? `${window.location.protocol}//${window.location.host}/`.replace('3000', '4000')
+  ? import.meta.env.VITE_API_BASE ??
+    `${window.location.protocol}//${window.location.host}/`.replace('3000', '4000')
   : `${window.location.protocol}//${window.location.host}/`
